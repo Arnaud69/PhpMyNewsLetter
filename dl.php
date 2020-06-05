3cacaf348cb3cdec59f5fd9cde1ab4427cd4ca1d
Arnaud <arnaud.gueneley@gmail.com>
2020-06-05 10:55:34 +0200

    Update dl.php
    review for session and __DIR__

--- dl.php
+++ dl.php
@@ -1,39 +1,28 @@
 <?php
-session_start();
 if(!file_exists("include/config.php")) {
 	header("Location:install.php");
 	exit;
 } else {
 	include("_loader.php");
-}
-
-$token=(empty($_GET['token'])?"":$_GET['token']);
-if(!isset($token) || $token=="")$token=(empty($_GET['token'])?"":$_GET['token']);
-if(tok_val($token)){
-	if(!checkAdminAccess($row_config_globale['admin_pass'],$form_pass)) {
-		if(!empty($_POST['form'])&&$_POST['form'])
-			header("Location:login.php?error=1");
-		else
-			header("Location:login.php");
+	$token=(empty($_GET['token'])?"":$_GET['token']);
+	if(!tok_val($token)){
+		header("Location:login.php?error=2");
 		exit;
 	}
-} else {
-	header("Location:login.php?error=2");
-	exit;
 }
-
+ob_start();
 $log =(empty($_GET['log'])?"":urldecode($_GET['log']));
-if(file_exists($log)){		 
+if(file_exists(__DIR__ . '/' .$log)){		 
 	header("Pragma: public");
 	header("Expires: 0"); 
 	header("Cache-Control: must-revalidate, post-check=0, pre-check=0"); 
 	header("Cache-Control: private",false);
 	header("Content-Type: text/plain\n");
-	header("Content-disposition: attachment; filename=".str_replace("logs/","",$log));
+	header("Content-disposition: attachment; filename=" . str_replace("logs/","",$log));
 	header("Content-Transfer-Encoding: binary"); 
-	header("Content-Length: ".filesize($log));
+	header("Content-Length: " . filesize(__DIR__ . '/' .$log));
 	header("Pragma: no-cache");
 	ob_clean(); 
-	flush();
-	readfile_chunked($log);
+	ob_end_flush();
+	readfile_chunked(__DIR__ . '/' . $log);
 }
